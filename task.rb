b231da5d1f920a7781df308d14ad7bab70b08163
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-09-02 21:09:38 +0900

    Q1,Q4,Q7の改善

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.push("斎藤")
+  names.<<("斎藤")
   puts names
 end
 
@@ -29,7 +29,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.delete(nil)
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -55,7 +55,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  array.map!{|n| n.to_i}
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
