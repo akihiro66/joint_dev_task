56994757c28b9e0da7c2c35eabd24952fc4f8676
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-09-03 12:14:24 +0900

    Q8~Q11の回答

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.<<("斎藤")
+  names << "斎藤"
   puts names
 end
 
@@ -64,7 +64,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -74,21 +75,38 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  # names.each.with_index(1) do |user, i|
+  #   puts "会員No.#{i} #{user}さん"
+  # end
+  # 省略形
+  names.each.with_index(1) {|user, i| puts "会員No.#{i} #{user}さん"}
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+    if food.include?("うに")
+      puts "#{food}は好物です"
+    else
+      puts "#{food}はまぁまぁ好きです"
+    end
+  end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  sports.flatten!.uniq!
 
+  text = "ユーザーの趣味一覧\n"
+  sports.each.with_index(1) do |sport, i|
+    text += "No#{i} #{sport}\n"
+  end
+
+  puts text
 end
 
 def q12
