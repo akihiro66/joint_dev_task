233322a5cfa430d8d4af3d077dedd579149d5795
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-09-08 10:07:27 +0900

    Q17,Q18の回答

--- task.rb
+++ task.rb
@@ -168,7 +168,19 @@ end
 
 class UserQ17
   # 以下に回答を記載
+  attr_accessor :name, :age, :gender
 
+  def initialize(name: name, age: age, gender: gender)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+
+  def info
+    puts "名前:#{@name}"
+    puts "年齢:#{@age}"
+    puts "性別:#{@gender}"
+  end
 end
 
 def q17
@@ -183,7 +195,20 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  attr_accessor :name, :age
 
+  def initialize(name: name, age: age)
+    @name = name
+    @age = age
+  end
+
+  def introduce
+    if @age < 20
+      puts "はいさいまいど〜,#{@name}です!!!"
+    else
+      puts "こんにちは,#{@name}と申します。宜しくお願いいたします。"
+    end
+  end
 end
 
 def q18
