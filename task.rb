a8be8872071443ba03fae46fe7614dc7588e7e5c
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-09-11 22:16:12 +0900

    Q20の再修正

--- task.rb
+++ task.rb
@@ -252,7 +252,7 @@ class Zoo
   end
 
   def info_entry_fee(user)
-    fee = case user.age
+    entry_fee = case user.age
     when 0..5
       @entry_fee[:infant]
     when 6..12
@@ -262,7 +262,7 @@ class Zoo
     when 65..120
       @entry_fee[:senior]
     end
-    puts "#{user.name}さんの入場料金は #{fee} 円です。"
+    puts "#{user.name}さんの入場料金は #{entry_fee} 円です。"
   end
 end
 
